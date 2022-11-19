79860183ecae06d8ef935b9fc28d91352d56a3cd
Hager Abdel Rahman <114147968+hagerahned@users.noreply.github.com>
2022-11-19 22:26:39 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+
+const puttonEL = document.querySelector(".putton");
+
+puttonEL.addEventListener("mouseover",(event)=>{               
+    const x = (event.pageX - puttonEL.offsetLeft);  
+    const y = (event.pageY - puttonEL.offsetTop);  
+    
+    puttonEL.style.setProperty("--xPos", x + "px")
+    puttonEL.style.setProperty("--yPos", y + "px")
+})
+
+
+//   step1:-
+//   document page HTML 
+//   querySelector :- you summon an element using its class, id,or tag itself
+//   step1: A variable identifier and a writer inside the class name  
+
+//   step2:-
+//   addEventListener :-you add function with action
+//   useing condation
+//   offsetLeft,offsetTop :- a property that returns the element in pixels OR the value returned 
+//   includes top and left, position and margin of the element, top and left padding, scrollbar,borders, and parents
+//  event الحدث
+ 
